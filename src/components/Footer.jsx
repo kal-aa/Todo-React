f93422eee96b0f01991b545be1e416aff1f17502
kal <sadkalshayee@gmail.com>
2024-12-17 10:59:51 -0800

    added margin-top to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center bg-red-500 text-white h-24 transition-all duration-100">
+    <footer className="flex justify-center items-center bg-red-500 text-white h-24 transition-all duration-100 mt-5">
       <NavLink to="/contact-us" className="hover:text-blue-200">
         Contact ME!
       </NavLink>
